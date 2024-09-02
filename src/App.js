49122c37ad2b35e31076e43469709a27d894b140
Adityagaurkar09 <adityagaurkar09@gmail.com>
2024-09-02 14:13:35 +0530

    quantity counter app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import './App.css'
 import bdyCake from "./birthday-cake.jpg"
 import bdyCakee from "./birthday-cakee.png"
@@ -8,15 +8,31 @@ import { type } from '@testing-library/user-event/dist/type'
 
 function App() {
 
+const SAMOSA_PRICE = 12 ; 
 const [quantity , setQuantity] = useState (1);
+const [totalBill , setTotalBill] = useState (SAMOSA_PRICE);
 
 const updateQuantity = (type) => {
 if(type === "plus"){
   setQuantity(quantity+1);
 }else if(type === "minus"  && quantity > 1){
   setQuantity(quantity-1)
+}}
+
+useEffect (()=>{
+
+  const Toatal = (SAMOSA_PRICE*quantity);
+  
+if(quantity < 10){
+  setTotalBill(Toatal);
 }
+else if(quantity >= 10 && quantity <= 20){
+  setTotalBill(Toatal - 20);
 }
+else { setTotalBill(Toatal - 50);
+  
+}
+},[quantity]) 
 
   return (
     <div className='card'>
@@ -25,7 +41,13 @@ if(type === "plus"){
       <p className='card-description'>
       Cake is a sweet, baked dessert that comes in various flavors and textures, 
       typically made from flour, sugar, eggs, and  oil.</p>
-      <p className='card-price'> PRICE ₹ 20</p>
+      
+      <div className='price'>
+      <p className='card-price'> PRICE ₹ {SAMOSA_PRICE}</p>
+      <p className='total-price'> TOTAL ₹ {totalBill}</p>
+      </div>
+
+      <p className='card-error'>{quantity > 10 ?  "bulk order hai time lagenga": null }</p>
 
       <div className='sing-container'>
         <img src={minussign} className='sign' alt="minus"
